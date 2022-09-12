953e38081b3065466c79c0ee9e37a71b2cd4f737
praveenbothithub <113330328+praveenbothithub@users.noreply.github.com>
2022-09-12 11:20:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,10 +43,10 @@ global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
+global.owner = ['94717189173']
 global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
+global.botnumber = '94717189173' //Add your Bot Number For Log
+global.botname = 'CYBER-X BETA Praveen' //ur bot name
 global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
 global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
