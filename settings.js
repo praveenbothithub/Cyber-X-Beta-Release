c1c4840d815fae7628b9557e287983d97d2c77b6
praveenbothithub <113330328+praveenbothithub@users.noreply.github.com>
2022-09-18 07:29:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -37,7 +37,7 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
 global.vcardowner = ['94763284510'] //ur owner number
-global.ownername = "Praveen" //ur owner name
+global.ownername = "Praveen Nimsara" //ur owner name
 global.ytname = "YT: TGM Arcade" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
